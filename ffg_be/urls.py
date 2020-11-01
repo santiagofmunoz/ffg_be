1e7d70c7586e88f755ff76e1bdded9bb90785571
Santiago Muñoz <santiagofmunoz@gmail.com>
2020-11-01 19:21:33 -0300

    NEW: Created get_formations to be able to get the list of all formations in the system NEW: Created export_formation_as_pdf to be able to export the information of the formation in a PDF. NEW: Added Reportlab and Pillow as libraries to manage the creation of PDFs NEW: Added the base image of the field to later be added to PDFs UPD: Updated migrations. UPD: Updated requirements.

--- ffg_be/urls.py
+++ ffg_be/urls.py
@@ -9,4 +9,6 @@ urlpatterns = [
     path('api/formation/create_formation/', formation_views.create_formation),
     path('api/formation/get_positions/', formation_views.get_positions),
     path('api/formation/get_players_position_detail/', formation_views.get_players_position_detail),
+    path('api/formation/get_formations/', formation_views.get_formations),
+    path('api/formation/export_formation_as_pdf/<int:formation_id>', formation_views.export_formation_as_pdf),
 ]
--- formation/views.py
+++ formation/views.py
@@ -1,8 +1,38 @@
+import io
+from PIL import Image
+from django.http import FileResponse
+from reportlab.lib.units import mm
+from reportlab.pdfgen import canvas
 from rest_framework import status
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
 
 from .serializers import *
+
+
+def position_switcher(position_name):
+    switcher = {
+        "Golero": [250, 750],
+        "Carrilero izquierdo": [430, 580],
+        "Carrilero derecho": [100, 580],
+        "Lateral izquierdo": [430, 650],
+        "Lateral derecho": [100, 650],
+        "Central izquierdo": [330, 650],
+        "Central derecho": [190, 650],
+        "Central": [250, 680],
+        "Mediocampista defensivo": [250, 500],
+        "Interior izquierdo": [430, 430],
+        "Interior derecho": [100, 430],
+        "Mediocentro izquierdo": [330, 430],
+        "Mediocentro derecho": [180, 430],
+        "Media punta": [250, 340],
+        "Delantero central": [250, 200],
+        "Volante izquierdo": [350, 250],
+        "Volante derecho": [170, 250],
+        "Extremo izquierdo": [430, 250],
+        "Extremo derecho": [100, 250],
+    }
+    return switcher.get(position_name, [0, 1])
 
 
 @api_view(['POST'])
@@ -46,3 +76,45 @@ def get_positions(request):
     positions = Position.objects.all()
     serializer = PositionSerializer(positions, many=True)
     return Response(serializer.data)
+
+
+@api_view(['GET'])
+def get_formations(request):
+    formations = Formation.objects.all()
+    serializer = FormationSerializer(formations, many=True)
+    return Response(serializer.data)
+
+
+@api_view(['GET'])
+def export_formation_as_pdf(request, formation_id):
+    # Se crea un buffer para crear la información del PDF
+    buffer = io.BytesIO()
+    # Se crea el PDF, utilizando el buffer y su archivo.
+    p = canvas.Canvas(buffer)
+    # Se dibuja la cancha de fútbol
+    cancha_futbol = Image.open("res/img/cancha_futbol.jpeg")
+    p.drawInlineImage(cancha_futbol, 26 * mm, 24 * mm)
+
+    formation_players = PlayerFormation.objects.all().filter(formation_id=formation_id)
+    serialized_formation_players = PlayerFormationSerializer(formation_players, many=True)
+    for player in serialized_formation_players.data:
+        player_id = player['player']
+        get_player = Player.objects.get(player_id=player_id)
+        serialized_get_player = PlayerSerializer(get_player)
+        player_obj = serialized_get_player.data
+        player_position_id = player_obj['position']
+        get_position = Position.objects.get(position_id=player_position_id)
+        serialized_get_position = PositionSerializer(get_position)
+        position_obj = serialized_get_position.data
+        position_name = position_obj['position_name']
+        xy_values = position_switcher(position_name)
+        player_name = player_obj['player_first_name'] + " " + player_obj['player_last_name']
+        p.drawString(xy_values[0], xy_values[1], player_name)
+
+    # Se cierra el PDF
+    p.showPage()
+    p.save()
+
+    # Se retorna el PDF al cliente
+    buffer.seek(0)
+    return FileResponse(buffer, as_attachment=False, filename='formacion.pdf')
